021a0e791fe824702963882641ae2ce5185d7fbc
Alina Abidi <alinaf@fb.com>
2017-06-21 15:14:25 -0700

    Set up network request

---
+++ movieGuide/NowPlayingViewController.swift
@@ -0,0 +1,76 @@
+//
+//  NowPlayingViewController.swift
+//  movieGuide
+//
+//  Created by Alina Abidi on 6/21/17.
+//  Copyright © 2017 Alina Abidi. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=3a8164978916a740e3977e79afd28289")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns (they are asynchronus)
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print (title)
+                }
+            }
+            
+        }
+
+       task.resume()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
